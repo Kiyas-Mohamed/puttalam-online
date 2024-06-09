12ea4796d11d0b1da59df0ea869bd739046837d8
Kiyas-Mohamed <darkdoor77@gmail.com>
2024-06-09 15:54:49 +0530

    Initial Commit

---
+++ assets/js/contect.js
@@ -0,0 +1,24 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict';
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation');
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms).forEach((form) => {
+      form.addEventListener('submit', (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  })();
+
+//   // Alert & Console Log
+// alert("Hi sir welcome to my page");
+// alert("Hi guys and you so welcome");
+// console.log("Hi sir welcome to my page");
+// console.log("Hi guys and you so welcome");
---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+// Alert & Console Log
+alert("Hi sir welcome to my page");
+alert("Hi guys and you so welcome");
+console.log("Hi sir welcome to my page");
+console.log("Hi guys and you so welcome");
